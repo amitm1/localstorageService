4b727d33698b93dab4281335d2ff24b47f9ed70c
amit_mishra <amit.mishra@fox.com>
2015-10-15 12:29:11 -0700

    js getValue

--- js/localStorageService.js
+++ js/localStorageService.js
@@ -4,3 +4,9 @@ var mystorage = localStorage;
 mystorage.setItem(key, value);
 return true
 }
+
+var getValue = function(key){
+var mystorage = localStorage;
+var value = mystorage.getItem(key);
+return value;
+}
